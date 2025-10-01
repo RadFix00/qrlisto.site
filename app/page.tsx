0d25fc705d5b700a9bc89e7381da35afd3e69d8b
RadFxpp <nicolas@paintpower.net>
2025-09-30 22:38:06 -0500

    boton

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
+      <h1>Genera tu QR Free!</h1>
+
+      <div style={{ backgroundColor: "black", padding: "20px", marginTop: "20px" }}>
+        <button>
+          generar QR
+        </button>
+        
+      </div>
       
     </div>
   );
