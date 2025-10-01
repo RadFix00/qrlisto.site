5b469a12d9d098c0a808d23861e20c72ea9086c0
RadFxpp <nicolas@paintpower.net>
2025-10-01 16:58:35 -0500

    update

--- app/src/components/generatorqr.tsx
+++ app/src/components/generatorqr.tsx
@@ -6,7 +6,33 @@ import styles from './generator.module.css';
 export default function Generatorqr() {
     return (
         <div className={styles.contentqr}>
-            Hola we :b
-        </div>
+            <div>
+                <h2>QrListo</h2>
+                <h1>Generador de QR's</h1>
+                <div style={{ display: 'flex', flexDirection: 'row', gap: '1em' }}>
+                    <div>
+                        <h3>Crea tu QR en base a un enlace</h3>
+                        <input type="text" 
+                            placeholder="Ingresa tu enlace aqui"
+                        />
+                        <button>Generar QR</button>
+                    </div>
+                    <div>
+                        <h3>Crea tu QR en base a un enlace</h3>
+                        <input type="text" 
+                            placeholder="Ingresa tu enlace aqui"
+                        />
+                        <button>Generar QR</button>
+                    </div>
+                    <div>
+                        <h3>Crea tu QR en base a un enlace</h3>
+                        <input type="text" 
+                            placeholder="Ingresa tu enlace aqui"
+                        />
+                        <button>Generar QR</button>
+                    </div>
+                </div>
+            </div>
+        </div>  
     )
 }
